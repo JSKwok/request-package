c80d9a50729c73d13206278394938c51968f1276
Justin <justin.s.kwok@gmail.com>
2019-05-06 20:34:57 +0000

    Created GET request for an image using chained functions in the request package

---
+++ request.js
@@ -0,0 +1,20 @@
+var request = require('request');
+var fs = require('fs');
+
+var query = 'https://sytantris.github.io/http-examples/future.jpg'
+
+request.get(query)
+  .on('error', function (err) {
+    throw err;
+  })
+  .on('response', function (response) {
+    console.log('Response status message: ', response.statusMessage);
+    console.log('Response status code: ', response.statusCode);
+    console.log('Content type: ', response.headers['content-type'])
+    console.log('Downloading image...')
+  })
+  .pipe(fs.createWriteStream('./future.jpg'))
+
+  .on('finish', function () {
+    console.log('Download complete.');
+  })
